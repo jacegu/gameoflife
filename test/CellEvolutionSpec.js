2af22f522252120e2137696d5b5545cf735dedae
Javier Acero <j4cegu@gmail.com>
2012-12-12 21:46:10 +0100

    custom matchers added

--- test/CellEvolutionSpec.js
+++ test/CellEvolutionSpec.js
@@ -1,43 +1,50 @@
-describe('an alive cell evolving', function() {
-  it('gets killed if it has no neighbours', function(){
-    var neighbourCount = 0
-    expect(isAlive(evolveCell(cell(), neighbourCount))).toBe(false);
-  });
-
-  it('gets killed if it has only 1 neighbour', function(){
-    var neighbourCount = 1
-    expect(isAlive(evolveCell(cell(), neighbourCount))).toBe(false);
-  });
-
-  it('remains alive if it has 2 neighbours', function(){
-    var neighbourCount = 2
-    expect(isAlive(evolveCell(cell(), neighbourCount))).toBe(true);
-  });
-
-  it('remains alive if it has 3 neighbours', function(){
-    var neighbourCount = 3
-    expect(isAlive(evolveCell(cell(), neighbourCount))).toBe(true);
-  });
-
-  it('gets killed if it has more than 3 neighbours', function(){
-    var neighbourCount = 4
-    expect(isAlive(evolveCell(cell(), neighbourCount))).toBe(false);
-  });
-});
-
-describe('a dead cell', function() {
-  describe('evolving with 3 neighbours', function() {
-    it('reborns', function() {
-      var neighbourCount = 3
-      expect(isAlive(evolveCell(deadCell(), neighbourCount))).toBe(true);
+describe('evolution of', function() {
+  beforeEach(function() {
+    this.addMatchers({
+      toBeAlive: function() {
+        return isAlive(this.actual);
+      },
+      toBeDead: function(expected) {
+        return !isAlive(this.actual);
+      }
     });
   });
-  describe('evolving with other than 3 neighbours alive', function() {
-    it('reborns', function() {
+  describe('an alive cell', function() {
+    it('kills it if it has no neighbours', function(){
+      var neighbourCount = 0
+      expect(evolveCell(cell(), neighbourCount)).toBeDead();
+    });
+
+    it('kills it if it has only 1 neighbour', function(){
+      var neighbourCount = 1
+      expect(evolveCell(cell(), neighbourCount)).toBeDead();
+    });
+
+    it('keeps it alive if it has 2 neighbours', function(){
       var neighbourCount = 2
-      expect(isAlive(evolveCell(deadCell(), neighbourCount))).toBe(false);
+      expect(evolveCell(cell(), neighbourCount)).toBeAlive();
+    });
+
+    it('keeps it alive if it has 3 neighbours', function(){
+      var neighbourCount = 3
+      expect(evolveCell(cell(), neighbourCount)).toBeAlive();
+    });
+
+    it('kills it if it has more than 3 neighbours', function(){
+      var neighbourCount = 4
+      expect(evolveCell(cell(), neighbourCount)).toBeDead();
+    });
+  });
+  describe('a dead cell', function() {
+    it('reborns it with 3 neighbours', function() {
+      var neighbourCount = 3
+      expect(evolveCell(deadCell(), neighbourCount)).toBeAlive();
+    });
+    it('keeps it dead with other than 3 neighbours', function() {
+      var neighbourCount = 2
+      expect(evolveCell(deadCell(), neighbourCount)).toBeDead();
       neighbourCount = 4
-      expect(isAlive(evolveCell(deadCell(), neighbourCount))).toBe(false);
+      expect(evolveCell(deadCell(), neighbourCount)).toBeDead();
     });
   });
 });
